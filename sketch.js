318b2e9fdf0d23d54f386cc387bb9466081a2517
xbolt12345 <75019744+xbolt12345@users.noreply.github.com>
2022-01-22 16:43:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var car, wall;
+var speed, weight;
+
+
+function setup() {
+  createCanvas(1600,400);
+  speed = random(55,90);
+  weight = random(400,1500);
+  car = createSprite(50, 200, 50, 50);
+  car.velocityX = speed;
+  car.shapeColor = "White";
+  wall = createSprite(1000, 200, 60, height/2);
+  wall.shapeColor = "Gray";
+  
+}
+
+function draw() {
+  background(0); 
+
+  if(wall.x-car.x<(car.width+wall.width/2)) {
+car.velocityX=0;
+var deformation = 0.5*weight*speed*speed/22500;
+if(deformation>180){
+car.shapeColor = "red";
+}
+if(deformation<180 && deformation>100){
+car.shapeColor = "Yellow";
+}
+if(deformation<100){
+  car.shapeColor = "Green";
+}
+}
+
+  drawSprites();
+}
